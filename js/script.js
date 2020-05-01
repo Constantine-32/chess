85797a40e50706a4ffbfee58cdac93ccd0376caf
Christian C. Romero <id.32@outlook.com>
2020-05-02 00:17:56 +0200

    Transitioned code to OOP

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
+// Clasess
+function State(board) {
+  this.board = board
+}
+
+function Game(board) {
+  this.state = new State(board)
+  this.states = [this.state]
+
+  this.generateBoard = function () {
+    const colors = {w: 'white', b: 'black'}
+    const pieces = {p: 'pawn', r: 'rook', h: 'knight', b: 'bishop', q: 'queen', k: 'king'}
+    const board = document.querySelector('.board')
+    const size = 90
+    for (let y = 0; y < 8; y++) {
+      for (let x = 0; x < 8; x++) {
+        const data = this.get(y, x)
+        if (!data) continue
+        const piece = document.createElement('piece')
+        piece.classList.add(colors[data[0]])
+        piece.classList.add(pieces[data[1]])
+        translate(piece, x * size, y * size)
+        board.appendChild(piece)
+      }
+    }
+  }
+
+  this.get = function (y, x) {
+    return this.state.board[y][x]
+  }
+}
+
 // Global variables
-const board = [
+const game = new Game([
   ['br','bh','bb','bq','bk','bb','bh','br'],
   ['bp','bp','bp','bp','bp','bp','bp','bp'],
   [ '' , '' , '' , '' , '' , '' , '' , '' ],
@@ -8,39 +40,20 @@ const board = [
   [ '' , '' , '' , '' , '' , '' , '' , '' ],
   ['wp','wp','wp','wp','wp','wp','wp','wp'],
   ['wr','wh','wb','wq','wk','wb','wh','wr']
-]
+])
 
-const colors = {w: 'white', b: 'black'}
-const pieces = {p: 'pawn', r: 'rook', h: 'knight', b: 'bishop', q: 'queen', k: 'king'}
-
-const htmlboard = document.querySelector('.board')
+game.generateBoard()
 
 let pieceD
 let pieceDX
 let pieceDY
 
 // Event listeners
-htmlboard.addEventListener('mousedown', dragStart)
-htmlboard.addEventListener('mousemove', drag)
-htmlboard.addEventListener('mouseup', dragEnd)
+document.body.addEventListener('mousedown', dragStart, false)
+document.body.addEventListener('mouseup', dragEnd, false)
+document.body.addEventListener('mousemove', drag, false)
 
 // Functions
-function fillBoard() {
-  const size = 90
-  for (let y = 0; y < 8; y++) {
-    for (let x = 0; x < 8; x++) {
-      const data = board[y][x]
-      if (!data) continue
-      const piece = document.createElement('piece')
-      piece.classList.add(colors[data[0]])
-      piece.classList.add(pieces[data[1]])
-      translate(piece, x * size, y * size)
-      htmlboard.appendChild(piece)
-    }
-  }
-}
-
-fillBoard()
 
 function dragStart(e) {
   if (e.target.tagName !== 'PIECE') return
@@ -55,6 +68,7 @@ function dragStart(e) {
 
 function drag(e) {
   if (!pieceD) return
+  e.preventDefault()
   let x = e.clientX - pieceD.parentNode.offsetLeft
   let y = e.clientY - pieceD.parentNode.offsetTop
   translate(pieceD, x - 45, y - 45)
