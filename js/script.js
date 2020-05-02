77b081b6ca985c0cc34cd923d48632fa7bf6ace4
Christian C. Romero <id.32@outlook.com>
2020-05-02 15:11:20 +0200

    Transitioned code to OOP

--- js/script.js
+++ js/script.js
@@ -1,32 +1,109 @@
 // Clasess
 function State(board) {
   this.board = board
+
+  this.get = function (coord) {
+    return this.board[coord.y][coord.x]
+  }
 }
 
 function Game(board) {
+  this.color = 'w'
   this.state = new State(board)
   this.states = [this.state]
+
+  this.pieceSelected = undefined
+  this.pieceD = undefined
+  this.pieceDPos = undefined
 
   this.generateBoard = function () {
     const colors = {w: 'white', b: 'black'}
     const pieces = {p: 'pawn', r: 'rook', h: 'knight', b: 'bishop', q: 'queen', k: 'king'}
     const board = document.querySelector('.board')
-    const size = 90
     for (let y = 0; y < 8; y++) {
       for (let x = 0; x < 8; x++) {
-        const data = this.get(y, x)
+        const data = this.get({x: x, y : y})
         if (!data) continue
         const piece = document.createElement('piece')
         piece.classList.add(colors[data[0]])
         piece.classList.add(pieces[data[1]])
-        translate(piece, x * size, y * size)
+        this.translate(piece, {x: x * 90, y: y * 90}, false)
         board.appendChild(piece)
       }
     }
   }
 
-  this.get = function (y, x) {
-    return this.state.board[y][x]
+  this.select = function (coord) {
+    this.pieceSelected = coord
+  }
+
+  this.move = function (coord) {
+    console.log('move from ', this.pieceSelected, ' to ', coord)
+  }
+
+  this.get = function (coord) {
+    return this.state.get(coord)
+  }
+
+  this.dragStart = function (e) {
+    if (e.target.tagName !== 'PIECE') return
+    if (!e.target.classList.contains('white')) return
+    this.pieceD = e.target
+    this.select(this.getBoardCoords(e))
+    this.pieceD.style.zIndex = '4'
+    this.pieceDPos = this.getRoundedCoords(e)
+    this.translate(this.pieceD, this.getCoords(e), true)
+  }
+
+  this.drag = function (e) {
+    if (!this.pieceD) return
+    e.preventDefault()
+    this.translate(this.pieceD, this.getCoords(e), true)
+  }
+
+  this.dragEnd = function (e) {
+    if (!this.pieceD) return
+    this.pieceD.style.zIndex = '2'
+
+    // let x = e.clientX - this.pieceD.parentNode.offsetLeft
+    // let y = e.clientY - this.pieceD.parentNode.offsetTop
+    // if (this.isInBoard(x, y)) {
+    //   let boardX = x - x % 90
+    //   let boardY = y - y % 90
+    //   this.translate(this.pieceD, boardX, boardY)
+    // } else {
+    //   this.translate(this.pieceD, this.pieceDX, this.pieceDY)
+    // }
+
+    const co = this.getBoardCoords(e)
+
+    if (0 <= co.x && co.x < 8 && 0 <= co.y && co.y < 8) {
+      this.move(co)
+      this.translate(this.pieceD, this.getRoundedCoords(e), false)
+    }
+    else this.translate(this.pieceD, this.pieceDPos, false)
+    this.pieceD = undefined
+  }
+
+  this.getCoords = function (e) {
+    return {
+      x: e.clientX - this.pieceD.parentNode.offsetLeft,
+      y: e.clientY - this.pieceD.parentNode.offsetTop
+    }
+  }
+
+  this.getRoundedCoords = function (e) {
+    const co = this.getCoords(e)
+    return {x: co.x - co.x % 90, y: co.y - co.y % 90}
+  }
+
+  this.getBoardCoords = function (e) {
+    const co = this.getCoords(e)
+    return {x: Math.floor(co.x / 90), y: Math.floor(co.y / 90)}
+  }
+
+  this.translate = function (piece, coord, center) {
+    piece.style.transform = 'translate('+(coord.x - (center ? 45 : 0))+'px, '+(coord.y - (center ? 45 : 0))+'px)'
   }
 }
 
@@ -44,56 +121,7 @@ const game = new Game([
 
 game.generateBoard()
 
-let pieceD
-let pieceDX
-let pieceDY
-
 // Event listeners
-document.body.addEventListener('mousedown', dragStart, false)
-document.body.addEventListener('mouseup', dragEnd, false)
-document.body.addEventListener('mousemove', drag, false)
-
-// Functions
-
-function dragStart(e) {
-  if (e.target.tagName !== 'PIECE') return
-  pieceD = e.target
-  pieceD.style.zIndex = '4'
-  let x = e.clientX - pieceD.parentNode.offsetLeft
-  let y = e.clientY - pieceD.parentNode.offsetTop
-  pieceDX = x - x % 90
-  pieceDY = y - y % 90
-  translate(pieceD, x - 45, y - 45)
-}
-
-function drag(e) {
-  if (!pieceD) return
-  e.preventDefault()
-  let x = e.clientX - pieceD.parentNode.offsetLeft
-  let y = e.clientY - pieceD.parentNode.offsetTop
-  translate(pieceD, x - 45, y - 45)
-}
-
-function dragEnd(e) {
-  if (!pieceD) return
-  pieceD.style.zIndex = '2'
-  let x = e.clientX - pieceD.parentNode.offsetLeft
-  let y = e.clientY - pieceD.parentNode.offsetTop
-  if (isInBoard(x, y)) {
-    let boardX = x - x % 90
-    let boardY = y - y % 90
-    translate(pieceD, boardX, boardY)
-  } else {
-    translate(pieceD, pieceDX, pieceDY)
-  }
-  pieceD = undefined
-}
-
-// Helper functions
-function translate(e, x, y) {
-  e.style.transform = 'translate('+x+'px, '+y+'px)'
-}
-
-function isInBoard(x, y) {
-  return 0 <= x && x <= 720 && 0 <= y && y <= 720
-}
+document.body.addEventListener('mousedown', function (e) { game.dragStart(e) }, false)
+document.body.addEventListener('mouseup', function (e) { game.dragEnd(e) }, false)
+document.body.addEventListener('mousemove', function (e) { game.drag(e) }, false)
